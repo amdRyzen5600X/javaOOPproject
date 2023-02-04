ae3e2b0f7a6aa967f5f6f31699a66d1964204102
GEN1RAL <85440357+FalkoLink@users.noreply.github.com>
2023-02-04 21:24:04 +0600

    Main 0

--- src/main.java
+++ src/main.java
@@ -1,5 +1,25 @@
 public class main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do{
+            System.out.println("""
+                    █   █ █▀▀ █   ▄▀▀ ▄▀▄ █▄ ▄█ █▀▀     ▀█▀ ▄▀▄     ▄▀▄ ▀ ▀█▀ █ █    █▄ ▄█ ▄▀▄ ▄▀▄ █▀▄ █   █▀▀
+                    █ █ █ █▀▀ █ ▄ █   █ █ █ █ █ █▀▀      █  █ █     █▀█ █  █  █ █ ▀▀ █ █ █ █ █ █ █ █ █ █ ▄ █▀▀
+                     ▀ ▀  ▀▀▀ ▀▀▀  ▀▀  ▀  ▀   ▀ ▀▀▀      ▀   ▀      ▀ ▀ ▀  ▀   ▀     ▀   ▀  ▀   ▀  ▀▀  ▀▀▀ ▀▀▀
 
+                    Select sign in option!
+                    1) Registration
+                    2) Login
+                    3) Exit
+                    """);
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1 -> Enter.register(sc);
+                case 2 -> Enter.login(sc);
+                case 3 -> System.exit(0);
+                default -> System.out.println("Sorry, invalid value entered. Try again!\n");
+            }
+        }while (true);
     }
 }
